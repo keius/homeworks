2f33aab076f47351e5f49d30712379a30c54a872
Greg <waterparkmelon@gmail.com>
2017-07-11 23:31:11 -0700

    finished hw

--- W7D3/store/store.js
+++ W7D3/store/store.js
@@ -1,3 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import RootReducer from '../reducers/root_reducer';
+
+const configureStore = () => {
+  return createStore(RootReducer, applyMiddleware(thunk));
+};
