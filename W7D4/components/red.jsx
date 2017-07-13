ce2f0a1f2a0e65ca6923759c8fe65003bbd94a69
Greg <waterparkmelon@gmail.com>
2017-07-12 20:06:11 -0700

    finished rainbow routes

--- W7D4/components/red.jsx
+++ W7D4/components/red.jsx
@@ -8,12 +8,15 @@ class Red extends React.Component {
     return(
       <div>
         <h2 className="red"></h2>
-        {/* Links here */}
+        <NavLink exact to="/red" >Red Only</NavLink>
+        <NavLink to="/red/orange" >Add Orange</NavLink>
+        <NavLink to="/red/yellow" >Add Yellow</NavLink>
 
-        {/* Routes here */}
+        <Route path="/red/orange" component={Orange} />
+        <Route path="/red/yellow" component={Yellow} />
       </div>
     );
   }
-};
+}
 
 export default Red;
